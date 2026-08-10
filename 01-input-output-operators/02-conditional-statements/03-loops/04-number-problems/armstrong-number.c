3dc4a438274356bebc4df6a486388f0a5aa0c768
Gowri035 <agraharapugowri285@gmail.com>
2026-08-10 20:35:11 +0530

    Create armstrong-number.c

---
+++ 01-input-output-operators/02-conditional-statements/03-loops/04-number-problems/armstrong-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, original, digit, sum = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    original = n;
+    while (n != 0) {
+        digit = n % 10;
+        sum = sum + digit * digit * digit;
+        n = n / 10;
+    }
+    if (sum == original)
+        printf("Armstrong number");
+    else
+        printf("Not Armstrong");
+    return 0;
+}
