5dc6b429a6bb2c683ae1b487fcf8a409bf94fde5
Gowri035 <agraharapugowri285@gmail.com>
2026-08-11 11:09:37 +0530

    Add program to read and print an array of integers

---
+++ 01-input-output-operators/02-conditional-statements/03-loops/04-number-problems/05-patterns/06-arrays/read-and-print-array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int arr[5];
+    int i;
+    printf("Enter 5 elements: ");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements: ");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
