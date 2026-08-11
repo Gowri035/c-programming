aa667f0906c510711b23f9215625f4f529e3a9bc
Gowri035 <agraharapugowri285@gmail.com>
2026-08-11 12:00:34 +0530

    Create reverse-string.c

---
+++ 01-input-output-operators/02-conditional-statements/03-loops/04-number-problems/05-patterns/06-arrays/07-strings/reverse-string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, length = 0;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    while (str[length] != '\0') {
+        length++;
+    }
+    printf("Reverse = ");
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    return 0;
+}
